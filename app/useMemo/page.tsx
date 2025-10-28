c58e7dc5cbfeb62f3832366282a87bf724ed2aff
Lisa Gee <zhusufeng@gmail.com>
2025-10-27 22:09:21 -0700

    Update the text on pages

--- app/useMemo/page.tsx
+++ app/useMemo/page.tsx
@@ -4,16 +4,37 @@ import Memo from "./Memo";
 
 const UseMemoDemo = () => {
   return (
-    <main style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        padding: "20px",
+      }}
+    >
       <h2>Memoization Example</h2>
-      <div>
-        This is an example use case for React&apos;s <code>useMemo</code>. We
-        have 2 factorial calculators: the left is non-memoized; the right is
-        memoized. Whenever either calculator calculates the factorial of a
-        number, it is purposefully slow. However, when you enter a number to
-        filter, the non-memoized calculator will calculate the factorial again,
-        which will lag the UX whereas the memoized calculator will use the
-        memoized value, and the UX will be fast.
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+        }}
+      >
+        <p>
+          This is an example use case for React&apos;s <code>useMemo</code>. We
+          have 2 factorial calculators: the left is non-memoized; the right is
+          memoized.
+        </p>
+        <p>
+          Whenever either calculator calculates the factorial of a number, it is
+          purposefully slow.
+        </p>
+        <p>
+          However, when you enter a number to filter, the non-memoized
+          calculator will calculate the factorial again, which will lag the UX
+          whereas the memoized calculator will use the memoized value, and the
+          UX will be fast.
+        </p>
       </div>
 
       <div style={{ display: "flex", gap: "20px" }}>
